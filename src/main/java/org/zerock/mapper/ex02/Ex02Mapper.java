67f1e2cac998126bdc86cbfe8b24ecc388faa862
eunju <z0303u@gmail.com>
2022-05-13 03:11:58 +0900

    mapper

---
+++ src/main/java/org/zerock/mapper/ex02/Ex02Mapper.java
@@ -0,0 +1,10 @@
+package org.zerock.mapper.ex02;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface Ex02Mapper {
+	
+	@Select("SELECT CustomerName FROM Customers WHERE CustomerId = #{id}")
+	String selectNameById(int id);
+
+}
